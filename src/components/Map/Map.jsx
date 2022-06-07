bd701c8fe6dc4bfe4e01848c5b643bf3613e61d1
ZiedAllouen <allouen.mohamed@EPISOUSSE.COM.TN>
2022-06-07 10:15:06 +0100

    v3

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -6,12 +6,23 @@ import Rating from '@material-ui/lab';
 import useStyles from './styles.js'
 
   const Map =()=> {
-      const classes = useStyles
+      const classes = useStyles();
+      const isMobile = useMediaQuery('(min-width:600px)');
+      const coordinates = { lat:0 ,lng :0} 
       return (
-          <div>
-              <h1>
-                 Map
-              </h1>
+          <div className={classes.mapContainer}>
+              <GoogleMapReact bootstrapURLKeys={{key:'AIzaSyChyCEuhJF3KNkRPdaHot9zDB2y7CgMyTI'}}
+              defaultCenter ={coordinates}
+              center = {coordinates}
+              defaultZoom = {14}
+              margin = { [50,50,50,50]}
+              options = {''}
+              onChange = {''}
+              onChildClick = {''}
+              >
+
+              </GoogleMapReact>
+             
           </div>
       )
   }
