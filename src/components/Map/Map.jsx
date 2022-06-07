a1b4a9b978465f3181e511fef29d66ffecffead0
ZiedAllouen <allouen.mohamed@EPISOUSSE.COM.TN>
2022-06-07 09:33:41 +0100

    v3

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,5 +1,12 @@
 import React from 'react' ;
+import GoogleMapReact from 'google-map-react';
+import {Paper,Typography,useMediaQuery} from '@material-ui/core';
+import  LocationOnOutlinedIcon  from '@material-ui/icons/LocalActivityOutlined';
+import Rating from '@material-ui/lab';
+import useStyles from './styles.js'
+
   const Map =()=> {
+      const classes = useStyles
       return (
           <div>
               <h1>
